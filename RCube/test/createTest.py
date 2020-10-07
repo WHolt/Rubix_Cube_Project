0c24be5f32822086d9507371f32eaae5ab739e54
capta <capta@Quicksilver>
2020-10-06 19:38:36 -0500

    Red Light |

--- RCube/test/createTest.py
+++ RCube/test/createTest.py
@@ -9,8 +9,7 @@ import RCube.create as create
 class createTest(unittest.TestCase):
 #Happy path
     def test100_010_NominalValueOfFaces(self):
-        expectedResult = {'cube': '111111111222222222333333333444444444555555555666666666', 
-                          'integrity': '88D897BD22E132D21A538745E63995B07D7C52CE9617A0979520545753EE0DED', 'status':'ok'}
+        expectedResult = {'cube': '111111111222222222333333333444444444555555555666666666', 'integrity': '88D897BD22E132D21A538745E63995B07D7C52CE9617A0979520545753EE0DED', 'status':'ok'}
         parms = {'op': 'create', 'faces' : '123456' }
         actualResult = create._create(parms)
         self.assertDictEqual(expectedResult, actualResult)
