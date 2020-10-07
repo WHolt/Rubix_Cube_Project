ea317032975c47ec2fc56d27864538aa0034b8fe
capta <capta@Quicksilver>
2020-10-06 19:26:30 -0500

    Red Light |

--- RCube/create.py
+++ RCube/create.py
@@ -1,5 +1,20 @@
+from builtins import None
+
 def _create(parms):
-    #try:
-     #   if parms()
-    result = {'create': 'create stub'}
+    try:
+        if parms['faces'] == '' or parms['faces'] == None:
+            cube = 'gybwro'
+        else
+            cube = parm['faces']    
+    except:
+            cube = 'gybwro'    
+    output = ''
+    for face in cube:
+        if (not(len(cube) == 6):
+            return ('status': 'error: Incorrect length for faces') 
+        if (cube.count(face) > 1):
+                return ('status': 'error: Duplicate Faces')
+        output += (face*9)
+    bytestring = bytes['output'], 'utf-8')
+    result['integrity'] = hashlib.sha256(bytestring).hexdigest().upper()
     return result
