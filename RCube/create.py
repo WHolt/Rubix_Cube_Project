4b2dd0934d96abdf83c6cf3dab215f18644d6d70
capta <capta@Quicksilver>
2020-10-06 19:36:26 -0500

    Red Light |

--- RCube/create.py
+++ RCube/create.py
@@ -1,20 +1,21 @@
-from builtins import None
+import hashlib
 
 def _create(parms):
+    result = {}
     try:
-        if parms['faces'] == '' or parms['faces'] == None:
+        if (parms['faces'] == '' or parms['faces'] == None):
             cube = 'gybwro'
-        else
-            cube = parm['faces']    
+        else:
+            cube = parms['faces']    
     except:
             cube = 'gybwro'    
     output = ''
     for face in cube:
-        if (not(len(cube) == 6):
-            return ('status': 'error: Incorrect length for faces') 
+        if (not(len(cube) == 6)):
+            return {'status': 'error: Incorrect length for faces'} 
         if (cube.count(face) > 1):
-                return ('status': 'error: Duplicate Faces')
+                return {'status': 'error: Duplicate Faces'}
         output += (face*9)
-    bytestring = bytes['output'], 'utf-8')
+    bytestring = bytes(output, 'utf-8')
     result['integrity'] = hashlib.sha256(bytestring).hexdigest().upper()
     return result
