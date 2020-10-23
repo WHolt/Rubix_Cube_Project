f4020c640ab7bcadd9c11f2b0692daf0667a1497
capta <capta@Quicksilver>
2020-10-22 20:47:25 -0500

    Red Light |

--- RCube/test/checkTest.py
+++ RCube/test/checkTest.py
@@ -22,7 +22,7 @@ class Test(unittest.TestCase):
         
     def test100_030_NominalValueOfCrossesCube(self):
         expectedResult = {'status':'crosses'}
-        parms = {'op': 'check', 'cube': 'ybybbbybybrbrrrbrbwgwgggwgwgogooogogryryyyryrowowwwowo', 'integrity': 'CE16F6174A8E0339E556FFDD1358AA56A03B1EB548AC31E324E52AAD7DC8BEF9'}
+        parms = {'op': 'check', 'cube': 'ybybbbybybrbrrrbrbwgwgggwgwgogooogogryryyyryrowowwwowo', 'integrity': '3A2CA2368EDAB67D1EAB30A5DCA67757FC389AC2924E3EDAB522BAABF840320'}
         actualResult = check._check(parms)
         self.assertDictEqual(expectedResult, actualResult)
     
