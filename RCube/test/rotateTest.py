5f2dcf915ede394b2dc2eb8323f62ab6ba4194fe
Cameron <cwh0023@auburn.edu>
2020-12-01 15:53:01 -0600

    Red Light |

--- RCube/test/rotateTest.py
+++ RCube/test/rotateTest.py
@@ -29,11 +29,11 @@ class Test(unittest.TestCase):
 #Sad Path    
     def test900_010_CubeValueMissing(self):
         expectedResult = {'Status':'error: Cube value missing'}
-        parms = {'op': 'rotate', 'side':'u', 'integrity': '88D897BD22E132D21A538745E63995B07D7C52CE9617A0979520545753EE0DED'}
+        parms = {'op': 'rotate', 'side':'u', 'cube': '', 'integrity': '88D897BD22E132D21A538745E63995B07D7C52CE9617A0979520545753EE0DED'}
         actualResult = rotate._rotate(parms)
         self.assertDictEqual(expectedResult, actualResult)
         
-    def test900_020_NoCubeKey(self):
+    def test900_020_CubeKeyMissing(self):
         expectedResult = {'status':'error: Cube key missing'}
         parms = {'op': 'rotate', 'side':'u','integrity': '88D897BD22E132D21A538745E63995B07D7C52CE9617A0979520545753EE0DED'}
         actualResult = rotate._rotate(parms)
